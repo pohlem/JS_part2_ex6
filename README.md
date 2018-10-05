28853bfd087469f48d2b96d91d4d0609d9c40bbc
pohlem <pohle.maxime@gmail.com>
2018-10-05 16:23:59 +0200

    Exercice 6 Finish

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_part2_ex1
+Exercice 1 of La-Manu JS Part 2
---
+++ function.js
@@ -0,0 +1,6 @@
+function change(i){
+document.getElementById('image'+i).src = "images/image"+i+"_2.jpg";
+}
+function returnBack(i){
+document.getElementById('image'+i).src = "images/image"+i+".jpg";
+}
